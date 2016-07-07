790faa44870b1c6d5f4312605639babca975aa06
Erik Berdonces Bonelo <sturgelose@gmail.com>
2016-07-07 19:07:46 +0200

    [NO TESTS] Implement basic sections

--- app/server/endpoints/sections/routes.js
+++ app/server/endpoints/sections/routes.js
@@ -1,12 +1,18 @@
 'use strict';
 
+const Boom = require('boom');
+const Joi = require('joi');
+
 module.exports = [
     {
         path: '/sections',
         method: 'GET',
         handler: (req, reply) => {
 
-            reply('List of all the sections');
+            const db = req.server.mongo.db;
+
+
+            reply(db.collection('sections').find({}).sort( { _id: 1 } ).toArray()).code(200);
         },
         config: {
             tags: ['api', 'swagger']
@@ -17,43 +23,94 @@ module.exports = [
         method: 'POST',
         handler: (req, reply) => {
 
-            reply('Create a section');
+            const db = req.server.mongo.db;
+
+            db.collection('sections')
+                .insertOne(req.payload)
+                .then(
+                    (result) => {
+
+                        reply(result.result).code(201);
+                    },
+                    (err) => {
+
+                        if (err) {
+                            if (err.code === 11000) {
+                                return reply(Boom.conflict('Duplicated index', err.errmsg));
+                            }
+                            return reply(Boom.internal('Internal MongoDB error', err.errmsg));
+                        }
+                    });
         },
         config: {
-            tags: ['api', 'swagger']
+            tags: ['api', 'swagger'],
+            validate: require('./validator')
         }
     },
     {
-        path: '/sections',
+        path: '/sections/{code}',
         method: 'DELETE',
         handler: (req, reply) => {
 
-            reply('Delete a section');
+            const db = req.server.mongo.db;
+
+            db.collection('sections')
+                .deleteOne({ _id: req.params.code })
+                .then(
+                    (result) => {
+
+                        if (result.result.n === 0) {
+                            reply(result).code(404); // If no items deleted, return a 404
+                        }
+                        reply(result).code(200);
+                    },
+                    (err) => {
+
+                        return reply(Boom.internal('Internal MongoDB error', err.errmsg));
+                    });
         },
         config: {
-            tags: ['api', 'swagger']
+            tags: ['api', 'swagger'],
+            validate: {
+                params: {
+                    code: Joi.string().regex(/^[A-Z]{2}-[A-Z]{2,4}$/).required().example('AA-AAAA')
+                        .description('Code of the section')
+                }
+            }
         }
     },
     {
-        path: '/sections',
+        path: '/sections/{code}',
         method: 'PUT',
         handler: (req, reply) => {
 
             reply('Update a section');
         },
         config: {
-            tags: ['api', 'swagger']
+            tags: ['api', 'swagger'],
+            validate: {
+                params: {
+                    code: Joi.string().length(2).uppercase().required().example('AA')
+                        .description('Code of the country to fetch')
+                }
+            }
         }
     },
     {
-        path: '/sections',
+        path: '/sections/{code}',
         method: 'PATCH',
         handler: (req, reply) => {
 
             reply('Update partially a section');
         },
         config: {
-            tags: ['api', 'swagger']
+            tags: ['api', 'swagger'],
+            validate: {
+                params: {
+                    code: Joi.string().length(2).uppercase().required().example('AA')
+                        .description('Code of the country to fetch')
+                }
+            }
         }
     },
     {
@@ -61,10 +118,30 @@ module.exports = [
         method: 'GET',
         handler: (req, reply) => {
 
-            reply('Information of section with code ' + req.params.code);
+            const db = req.server.mongo.db;
+
+            db.collection('sections')
+                .findOne({
+                    _id: req.params.code
+                })
+                .then(
+                    (result) => {
+
+                        reply(result).code(200);
+                    },
+                    (err) => {
+
+                        return reply(Boom.internal('Internal MongoDB error', err.errmsg));
+                    });
         },
         config: {
-            tags: ['api', 'swagger']
+            tags: ['api', 'swagger'],
+            validate: {
+                params: {
+                    code: Joi.string().regex(/^[A-Z]{2}-[A-Z]{2,4}$/).required().example('AA-AAAA')
+                        .description('Code of the section')
+                }
+            }
         }
     },
     {
--- app/server/endpoints/sections/validator.js
+++ app/server/endpoints/sections/validator.js
@@ -1,3 +1,16 @@
-/**
- * Created by sturgelose on 03/05/2016.
- */
+'use strict';
+
+const Joi = require('joi');
+
+module.exports = {
+    payload: {
+        _id: Joi.string().regex(/^[A-Z]{2}-[A-Z]{2,4}$/).example('AA-AAAA')
+            .description('Code of the section'),
+        url: Joi.string().uri().example('html://esnantartica.org')
+            .description('URL to the Country\'s ESN Homepage'),
+        name: Joi.string().required().example('ESN Antartica')
+            .description('Full name of the country'),
+        country: Joi.string().length(2).uppercase().required().example('AA')
+            .description('Code of the country')
+    }
+};

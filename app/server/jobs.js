62297246a36e132e75f90bf6929a651a00839f5c
Javier Rodriguez <javi.role1@gmail.com>
2016-07-11 17:02:10 +0200

    Fetch sections' job added as well ass its job to the cron scheduler

--- app/server/jobs.js
+++ app/server/jobs.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const FetchCountries = require('./jobs/fetchCountries');
+const FetchSections = require('./jobs/fetchSections');
 const Schedule = require('node-schedule');
 
 const ScheduleJobs = (server) => {
@@ -8,6 +9,7 @@ const ScheduleJobs = (server) => {
     server.log('info', 'Executing Scheduled Jobs at start');
 
     FetchCountries.schedule(server);
+    FetchSections.schedule(server);
 
     // ==================
     // = CRON SCHEDULES =
@@ -17,9 +19,13 @@ const ScheduleJobs = (server) => {
         '0 0 * * *',
         () => {
 
-            return FetchCountries.schedule(server);
-        });
+            return FetchCountries.schedule(server)
+                .then(() => FetchSections.schedule(server))
+                .catch((err) => {
+                    server.log('error', 'An error occurred while executing cron' + err);
+                });
 
+        });
 };
 
 module.exports = ScheduleJobs;

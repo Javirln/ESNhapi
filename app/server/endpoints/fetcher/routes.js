4e538a41e1ebc60b141dcfb7b819a7a6095de310
Javier Rodriguez <javi.role1@gmail.com>
2016-07-14 14:52:30 +0200

    First implementation of the Image fetcher So far, given a uri to an image and a name the app saves that image under the folder:
    /app/server/images
    
    Validation must be added

--- app/server/endpoints/fetcher/routes.js
+++ app/server/endpoints/fetcher/routes.js
@@ -1,14 +1,37 @@
 'use strict';
 
+const request = require('request');
+const fs = require('fs');
+const Joi = require('joi');
+
+const base_dir = './app/server/images/';
+
 module.exports = [
     {
-        path: '/fetchMe',
+        path: '/image',
         method: 'GET',
         handler: (req, reply) => {
+            const download = function (uri, filename, callback) {
+                request.head(uri, (err, res, body) => {
+                    // We must control whether the header content-type is included or not,
+                    // otherwise the file will not be saved correctly
+                    request(uri).pipe(fs.createWriteStream(
+                        base_dir + filename + '.' + res.headers['content-type'].split('/')[1]))
+                        .on('close', callback);
+                });
+            };
 
-            reply('Triggers an update to fetch from an specific origin');
+            download(req.query.url, req.query.name, () => {
+                reply('Image successfully downloaded').code(200);
+            });
         },
         config: {
+            validate: {
+                query: {
+                    url: Joi.string().required().description('Url to image'), // Validation for urls needs to be implemented
+                    name: Joi.string().required().description('Name of the file') // Validation for urls needs to be implemented
+                }
+            },
             tags: ['api', 'swagger']
         }
     }

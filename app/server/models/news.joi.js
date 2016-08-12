af479480eb75e047ebd0a7f3cb359fe9e22f89c4
Erik Berdonces Bonelo <sturgelose@gmail.com>
2016-08-12 19:14:06 +0200

    Implement news resource

---
+++ app/server/models/news.joi.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Joi = require('joi');
+const CountryCode = require('./country.joi').code;
+const CityCode = require('./city.joi').code;
+const SectionCode = require('./section.joi').code;
+
+const BaseSchema = Joi.object({
+    code: Joi.string().regex(/^[A-Z]{2}-[A-Z]{2,4}-[A-Z0-9]{3,4}$/)
+        .example('AA-AAAA-AAAA')
+        .description('Code of the section'),
+    title: Joi.string()
+        .example('Awesome title')
+        .description('Title of the new'),
+    content: Joi.any()
+        .example('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+        .description('The body of the new'),
+    createdOnSatellite: Joi.date()
+        .example('2016-06-05T12:32:35+02:00')
+        .description('Date when the new was created on Satellite'),
+    lastUpdate: Joi.number()
+        .example('1468776265810.0')
+        .description('The timestamp about when it was fetched'),
+    country: CountryCode,
+    city: CityCode,
+    section: SectionCode
+})
+    .label('New')
+    .requiredKeys('code', 'title', 'content', 'country', 'city', 'section', 'createdOnSatellite', 'lastUpdate');
+
+exports.base = BaseSchema;
+exports.code = Joi.reach(BaseSchema, 'code');
--- app/server/models/section.mongoose.js
+++ app/server/models/section.mongoose.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const Mongoose = require('mongoose');
-Mongoose.Promise = require('bluebird');
+const Promise = require('bluebird');
+Mongoose.Promise = Promise;
 const Joigoose = require('joigoose')(Mongoose);
 const MongooseHidden = require('mongoose-hidden')();
-const Schema = Joigoose.convert(require('./section.joi').base);
 
+/**
+ * Creates a Mongoose model
+ */
 const createModel = () => {
-// Extend Schema
+
+    // Convert it from Joi schema
+    const Schema = Joigoose.convert(require('./section.joi').base);
+
+    // Extend the schema
     Schema.code.unique = true;
     Schema.country = require('./country.mongoose').countryField;
     Schema.city = require('./city.mongoose').cityField;
@@ -32,4 +39,32 @@ const createModel = () => {
 };
 
 exports.Model = createModel();
+
+/**
+ * Creates a section field with validation
+ * that the section previously exists
+ */
+exports.sectionField = {
+    type: String,
+    ref: 'Section',
+    index: true,
+    validate: {
+        validator: (value, done) => {
+
+            const Section = require('./section.mongoose').Model;
+            Section.find({ code: value })
+                .then((result) => {
+
+                    if (result.length === 1) {
+                        done(true);
+                    }
+                    else {
+                        return Promise.reject();
+                    }
+                })
+                .catch(() => done(false));
+        },
+        message: '{VALUE} is not an existing Section code'
+    }
+};
 

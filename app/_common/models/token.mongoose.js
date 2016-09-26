81c11097b4e76eb01e906b99c500bdfe5ebee880
Erik Berdonces Bonelo <sturgelose@gmail.com>
2016-09-26 12:42:53 +0200

    Create Token schema and create basic token auth.
    Also, add it in Swagger (check GET /countries endpoint)
    Tokens need to be added manually in the DB, but in the future this can be improved

--- app/_common/models/token.mongoose.js
+++ app/_common/models/token.mongoose.js
@@ -1,5 +1,38 @@
 'use strict';
 
 const Mongoose = require('mongoose');
-const Promise = require('bluebird');
+const Schema = Mongoose.Schema;
 
+const tokenSchema = new Schema({
+    token: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    access_rights: {
+        type: String,
+        unique: true
+    },
+    created_at: Date,
+    updated_at: Date
+});
+
+// on every save, add the date
+tokenSchema.pre('save', function(next) {
+    // get the current date
+    const currentDate = new Date();
+
+    // change the updated_at field to current date
+    this.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+
+    next();
+});
+
+const Token = Mongoose.model('Token', tokenSchema);
+
+module.exports = Token;
--- app/api/config/auth/auth-simple-token.js
+++ app/api/config/auth/auth-simple-token.js
@@ -2,19 +2,32 @@
 
 const Joi = require('joi');
 
+const Token = require('../../../_common/models/token.mongoose');
+
 exports.setup = (server) => {
 
 
     server.auth.strategy('simple', 'bearer-access-token', {
-        validateFunc: (token, callback) => {
+        validateFunc: function (token, callback) {
 
-            // Use a real strategy here,
-            // comparing with a token from your database for example
-            if (token === '1234') {
-                return callback(null, true, { token: token }, { artifact1: 'an artifact' });
-            }
+            Token.find({ token: token })
+                .lean()
+                .exec()
+                .catch((error) => callback(error, false, { token: token }))
+                .then((valid_token) => {
 
-            return callback(null, false, { token: token }, { artifact1: 'an artifact' });
+                    if (valid_token && valid_token.length === 1 && valid_token[0].token === token) {
+
+                        if (this.method === 'get') {
+                            return callback(null, true, { token: token });
+                        }
+                        else if (valid_token[0].access_rights === 'ADMIN' && this.method !== 'get') {
+                            return callback(null, true, { token: token });
+                        }
+
+                        return callback(error, false, { token: token });
+                    }
+                });
         }
     });
 };
